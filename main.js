0414e4b6afed11bb2b4aaf050b648ec96c2cd600
rafaelkaaaa <147595141+rafaelkaaaa@users.noreply.github.com>
2023-11-21 13:44:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const emptyArray = [];
+
+const numbers = [1,2,3];
+
+const fruits = ['киви','банан','виноград'];
+
+console.log (fruits[0]);
+console.log (fruits[1]);
+
+
+
+for (let i=0;i<fruits.length;i++)[
+ console.log (fruits[i])   
+]
+
+const myclass1 = document.querySelector('.myclass1').textContent
+console.log(myclass1)
+
+let raf = []
+console.log (raf)
+raf.push(myclass1)
+console.log (raf)
+
+
+const myclass2 = document.querySelector('.myclass2').textContent
+console.log(myclass1)
+
+let miras = []
+console.log (miras)
+raf.push(myclass2)
+console.log (miras)
+
+const myclass3 = document.querySelector('.myclass3').textContent
+console.log(myclass3)
+
+let tima = []
+console.log (tima)
+raf.push(myclass1)
+console.log (tima)
